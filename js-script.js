b53de65d88b8a9aa9f5a2a7d6e5f689a7ed39364
Lera <ohhsith@ukr.net>
2021-09-29 16:32:15 +0300

    fixed

--- js-script.js
+++ js-script.js
@@ -5,9 +5,10 @@ let $input = $('#title');
 async function getTodos() {
     let todos = $.ajax({
         url: 'http://localhost:3000/todos',
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
         dataType: "json",
         success: function (data) {
-            return data;
+            return JSON.stringify(data);
         }
     });
     return todos;
@@ -16,9 +17,10 @@ async function getTodos() {
 function render(dataArray) {
     let listView = '';
     $.each(dataArray, function (obj, value) {
-        let status = value.completed === 'false' ? "in-progress" : "confirmed"
+        let status = value.completed === false ? "in-progress" : "confirmed"
         listView += ` <li class="${status}" data-id="${value.id}" data-title="${value.title}" data-complited="${value.completed}"><span>${value.title}</span><button type="submit" id="delete">delete</button></li> `
     })
+    
     $todoList.html(listView)
 }
 
@@ -32,28 +34,38 @@ $create.on('click',async function addTodo(e) {
     
     e.preventDefault();
       
-    $.post( 'http://localhost:3000/todos', { "title": $input.val(), "completed": false})
-        .done(function( data ) {
-            $todoList.append(`<li class="in-progress" data-title="${$input.val()}" data-complited="false"><span>${$input.val()}</span><button type="submit" id="delete">delete</button></li>`)
+    $.ajax({
+        url: 'http://localhost:3000/todos',
+        type: 'POST',
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
+        dataType: "json",
+        data: JSON.stringify({
+                "title": $input.val(),
+                "completed": false
+                }),
+        success: function (data) {
+            $todoList.append(`<li class="in-progress" data-title="${$input.val()}" data-complited="false" data-id="${data.id}"><span>${$input.val()}</span><button type="submit" id="delete">delete</button></li>`)
             $input.val(' ')
-        });
+        }
+    });
 
 })
 
 $todoList.on('click', 'span', async function changeTodoList() {
         let $elemId = $(this).parent().attr('data-id');
         let $elemTitle = $(this).parent().attr('data-title');
-        console.log($elemId)
+        // console.log($elemId)
 
 
         $.ajax({
             url: 'http://localhost:3000/todos/'+$elemId,
             type: 'PUT',
-            data: {
+            headers: {'Content-Type': 'application/json;charset=utf-8'},
+            data: JSON.stringify({
                         "title": $elemTitle,
                         "completed": true
-                    },
-         });
+                    }),
+        });
 
         $(this).addClass('confirmed')
 
@@ -66,6 +78,7 @@ $todoList.on('click', 'button', async function deleteTodo(e) {
     console.log($elemId)
     $.ajax({
         url: 'http://localhost:3000/todos/'+$elemId,
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
         type: 'DELETE'
         
      });
